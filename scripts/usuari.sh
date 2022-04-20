638b807975fb9606a961e5aa239f4b488253e742
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-04-20 16:48:54 +0200

    m06

---
+++ scripts/usuari.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Demana usuari
+echo "Indica el nom usuari del sistema"
+read usuari
+
+#Comprovació existencia del usuari
+if [ $(getent passwd $usuari) ] ; then
+        echo "L'usuari $usuari SÍ que existeix."
+        #llistar processos
+        echo "Els processos de l'usuari $usuari són els següents: \n"
+        ps -u $usuari
+else
+        echo "L'usuari $usuari NO existeix."
+fi
