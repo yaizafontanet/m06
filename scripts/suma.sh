00036676f1b9107b415fd1ee39292d98895cac0a
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-04-25 12:10:09 +0200

    m06

--- scripts/suma.sh
+++ scripts/suma.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-#Feu un shell script que sumi tots els números rebuts com a arguments i mostri el resultat per pantalla amb el missatge “La suma de tots els nombres rebuts per paràmetre és: <total>.” El nombre de paràmetres és indeterminat i cal comprovar que són números.
-
-arguments = $#
+numero='^[0-9]+$'
 sum=0
 
+#comprovació de que s'han indicat arguments
+if [$# != 0]; then
+    #bucle per realitzar la comprovació de que cada argument és un número
+    for arg in "$@"
+    do
+        #comprovació de que els arguments són números
+        if [$arg =~ $numero]; then
+            #bucle per realitzar la suma dels arguments
+            for n in "$@"
+            do
+                sum=$(($sum+$n))
+            done
 
-#bucle per realitzar la suma dels arguments
-for n in "$@"
-do
-    sum=$(($sum+$n))
-done
+            total = $sum
+            echo "La suma de tots els nombres rebuts per parametre és: $total "
 
-total = $sum
-echo "La suma de tots els nombres rebuts per parametre és: $total "
+        else
+            echo "L'argument $arg no és un número. Utilitza arguments númerics."
+        fi
+    done
+#missatge de sortida si no han indicat arguments
+else
+    echo "Has d'indicar els arguments amb els que vols treballar"
+fi
