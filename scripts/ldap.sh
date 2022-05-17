81ceb2ff7f05b25c2ca13c3af803a8b8236474a2
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-05-17 10:36:04 +0200

    m06

--- scripts/ldap.sh
+++ scripts/ldap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-R1=echo $1 | awk -F '://' '{print $1}'
-R2=echo $2 | awk -F '://' '{print $1}'
+R1=$(echo $1 | awk -F '://' '{print $1}')
+R2=$(echo $2 | awk -F '://' '{print $1}')
+
+n=100
+x=100
 
 #funció per un possible error en la referència del servidor LDAP o existencia del fitxer
 error_exit()
@@ -11,11 +14,15 @@ error_exit()
 
 proces()
 {
-    #Control per veure si el fitxer existeix i la pàgina està operativa
-    if [[ -f $fitxer ]] && [[ (curl -w $URI) 2>/dev/null ]]; then
+    #Control per veure si el fitxer existeix
+    if [[ -f $fitxer ]]; then
         #Bucle per llegir les linies del nostre arxiu
         while read -r line;
         do
+	    #Obtenir la descomposició del dc que indiquem com a paràmetre a l'execució del script
+	    dc1=$(echo $URI | awk -F '.' '{print $1}' | awk -F '://' '{print $2}')
+	    dc2=$(echo $URI | awk -F '.' '{print $2}')
+
             #Combinació de les lletres pel nom d'usuari
             #u1 agafem la primera lletra de la linia, en aquest cas la primera inicial del nom
             u1=$(echo $line | cut -b 1)
@@ -23,6 +30,9 @@ proces()
             u2=$(echo $line | awk '{print $2}' | cut -b 1,2,3,4)
             #user realitzem la combinació de l'enunciat i ho passem tot a minúscules
             user=$(echo $u2$u1 | tr '[:upper:]' '[:lower:]')
+	    #Obtenir nom i cognom que son la primera i segona paraula del nostre arxiu
+	    nom=$(echo $line | awk '{print $1}')
+	    cognom=$(echo $line | awk '{print $2}')
 
             #Agafar la última paraula de la linia, que es la contrasenya
             pass=$(echo $line | awk 'NF>1{print $NF}')
@@ -33,12 +43,33 @@ proces()
             #Agafar el quart paràmetre que es la adreça de correu
             address=$(echo $line | awk -v N=4 '{print $N}')
 
-            #Crear el usuari de ldap
-            
+	    n=$((n+1))
+	    x=$((x+1))
 
+            #Crear l'arxiu amb els paràmetres dels usuaris
+	    echo "dn: cn=$nom,ou=Usuaris,dc=$dc1,dc=$dc2
+objectClass: top
+objectClass: posixAccount
+objectClass: inetOrgPerson
+objectClass: person
+cn: $nom
+uid: $user
+uidNumber: $n
+gidNumber: $x
+homeDirectory: /home/$user
+loginShell: /bin/bash
+userPassword: $pass
+sn: $cognom
+mail: $address
+telephoneNumber: $tel
+	    " >> users.ldif
+	
         done < $fitxer
+
+	#Crear els usuaris de ldap a partir de l'arxiu
+        #$(ldapadd -x -D "cn=admin,dc=$dc1,dc=$dc2" -W -f users.ldif)
     else
-        echo error_exit
+	    error_exit
     fi
 }
 
