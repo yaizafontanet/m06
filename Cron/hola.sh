b779d1d26eb408043719af31b65ffd192b06f8a0
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-05-18 10:20:20 +0200

    m06

--- Cron/hola.sh
+++ Cron/hola.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-user=$(echo $HOSTNAME)
-echo "Hola $user"
+echo "Hola $USER"
 
 #Programa per que s'executi cada minut a traves de cron.
-# * * * * * ~/hola.sh >> ~/hola.log
+# * * * * * /home/yaiza/hola.sh >> /home/yaiza/logs/hola.log
 
 #1- Cada  dia a les 7 del matí
-# 0 7 * * * ~/hola.sh >> ~/hola1.log
+# 0 7 * * * /home/yaiza/hola.sh >> /home/yaiza/logs/hola1.log
 
 #2- Cada diumenge a les 7 de la tarda
-# 0 19 * * 7 ~/hola.sh >> ~/hola2.log
+# 0 19 * * 7 /home/yaiza/hola.sh >> /home/yaiza/logs/hola2.log
+# 0 19 * * 0 /home/yaiza/hola.sh >> /home/yaiza/logs/hola2.log
 
 #3- Cada 4 de febrer a les 7 de la tarda
-# 0 19 4 2 * ~/hola.sh >> ~/hola3.log
+# 0 19 4 2 * /home/yaiza/hola.sh >> /home/yaiza/logs/hola3.log
 
 #4- Cada 2 setmanes, divendres a les 9 de la nit
-# 0 21 * * 7 [ $(expr $(date +%W) % 2) -eq 1 ] && ~/hola.sh >> ~/hola4.log
+# 0 21 * * 5 [[ $(expr $(date +%W) % 2) -eq 0 ]] && /home/yaiza/hola.sh >> /home/yaiza/logs/hola4.log
 
 #5- Cada primer diumenge de cada mes
-# 00 09 * * 7 [ $(date +\%d) -le 07 ] && ~/hola.sh >> ~/hola5.log
+# 0 0 * * 7 [[ $(date +\%d) -le 07 ]] && /home/yaiza/hola.sh >> /home/yaiza/logs/hola5.log
+# 0 0 * * 0 [[ $(date +\%d) -le 07 ]] && /home/yaiza/hola.sh >> /home/yaiza/logs/hola5.log
--- Cron/hola2.sh
+++ Cron/hola2.sh
@@ -7,5 +7,4 @@ echo "Avui és $(date +%x)"
 echo "Són les $(date +%R)"
 echo "******************"
 
-#Fixeu una hora d’execució de manera que en pugueu comprovar el funcionament. Per exemple, si són les 19.00 h fixeu l’hora a les 19.10 h.
-#Redirigiu la sortida estàndard i la d’error del procés a un fitxer anomenat hola.log situat al vostre directori de treball.
+# 0 19 * * * /home/yaiza/hola2.sh >> /home/yaiza/logs/hola2.log
