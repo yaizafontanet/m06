84ff60dfd668fdc9b911b7dc4b6a21b3ae388103
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-05-23 18:11:58 +0200

    m06

--- Cron/hola2.sh
+++ Cron/hola2.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# hola.sh
 echo "******************"
 echo "Hola mon"
 echo "Sóc $USER"
 echo "Avui és $(date +%x)"
 echo "Són les $(date +%R)"
 echo "******************"
-
-# 0 19 * * * /home/yaiza/hola2.sh >> /home/yaiza/logs/hola2.log
