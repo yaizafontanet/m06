92a356b5de7e0ee9fde5ec5bdbff404471556202
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-05-25 16:31:27 +0200

    m06

--- Cron/Examen/underconstruction.sh
+++ Cron/Examen/underconstruction.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm /var/www/html/dominirandom.com/index.html
-mv /home/web/underconstruction/index.html /var/www/html/dominirandom.com/
+cp /home/web/underconstruction/index.html /var/www/html/dominirandom.com/
 systemctl restart apache2.service
